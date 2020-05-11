cc8afef55e150f01f5678bae8dad689b992859fc
stickler-ci <support@stickler-ci.com>
2020-05-11 09:30:22 +0000

    Fixing style errors.

--- test_exercise.py
+++ test_exercise.py
@@ -1,8 +1,10 @@
 from exercise import butterfly
 from exercise import vowelcount
 
+
 def test_butterfly():
     assert butterfly("abyss") == "abyff"
+
 
 def test_vowelcount():
     assert vowelcount("abyss", 1) == 2
